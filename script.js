17dca0b8c2fe6bd224ddf93b395936f8e7f842bf
Lucky Goswami <lgoswami204@gmail.com>
2024-03-13 14:07:25 +0530

    update: load only limited chats

--- script.js
+++ script.js
@@ -57,35 +57,75 @@ function loadData() {
       let chatsArray = Object.values(snapshot.val());
       chatArea.innerHTML = "";
 
-      for (let i = 0; i < chatsArray.length; i++) {
-        let chatUser = chatsArray[i].user;
-        let chatText = chatsArray[i].text;
-        let chatDateandTime = chatsArray[i].dateAndTime;
-        let chatTime = chatDateandTime.split(" ")[4];
+      // load only limited messages..
+      const chatLimit = 100;
+      if (chatsArray.length > chatLimit) {
+        for (
+          let i = chatsArray.length - 1;
+          i >= chatsArray.length - chatLimit;
+          i--
+        ) {
+          let chatUser = chatsArray[i].user;
+          let chatText = chatsArray[i].text;
+          let chatDateandTime = chatsArray[i].dateAndTime;
+          let chatTime = chatDateandTime.split(" ")[4];
 
-        let message_box = document.createElement("div");
-        message_box.classList.add("message-box");
-        chatArea.append(message_box);
+          let message_box = document.createElement("div");
+          message_box.classList.add("message-box");
+          chatArea.append(message_box);
 
-        let message = document.createElement("div");
-        message.classList.add("message", `${chatUser}`);
-        message_box.append(message);
+          let message = document.createElement("div");
+          message.classList.add("message", `${chatUser}`);
+          message_box.append(message);
 
-        let text = document.createElement("div");
-        text.classList.add("text");
-        // text.innerText = `${chatText} at  ${chatTime
-        //   .split(":")
-        //   .slice(0, 2)
-        //   .join(":")}`;
-        text.innerHTML = `${chatText} <span class="space">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span><span class='time'>${chatTime
-          .split(":")
-          .slice(0, 2)
-          .join(":")}</span>`;
-        message.append(text);
+          let text = document.createElement("div");
+          text.classList.add("text");
+          // text.innerText = `${chatText} at  ${chatTime
+          //   .split(":")
+          //   .slice(0, 2)
+          //   .join(":")}`;
+          text.innerHTML = `${chatText} <span class="space">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span><span class='time'>${chatTime
+            .split(":")
+            .slice(0, 2)
+            .join(":")}</span>`;
+          message.append(text);
 
-        // aligning msgs
-        if (currentUser == chatUser) {
-          message.style.justifyContent = "flex-end";
+          // aligning msgs
+          if (currentUser == chatUser) {
+            message.style.justifyContent = "flex-end";
+          }
+        }
+      } else {
+        for (let i = chatsArray.length - 1; i >= 0; i--) {
+          let chatUser = chatsArray[i].user;
+          let chatText = chatsArray[i].text;
+          let chatDateandTime = chatsArray[i].dateAndTime;
+          let chatTime = chatDateandTime.split(" ")[4];
+
+          let message_box = document.createElement("div");
+          message_box.classList.add("message-box");
+          chatArea.append(message_box);
+
+          let message = document.createElement("div");
+          message.classList.add("message", `${chatUser}`);
+          message_box.append(message);
+
+          let text = document.createElement("div");
+          text.classList.add("text");
+          // text.innerText = `${chatText} at  ${chatTime
+          //   .split(":")
+          //   .slice(0, 2)
+          //   .join(":")}`;
+          text.innerHTML = `${chatText} <span class="space">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span><span class='time'>${chatTime
+            .split(":")
+            .slice(0, 2)
+            .join(":")}</span>`;
+          message.append(text);
+
+          // aligning msgs
+          if (currentUser == chatUser) {
+            message.style.justifyContent = "flex-end";
+          }
         }
       }
 
